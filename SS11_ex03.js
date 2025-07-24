829527e9905555c377698dbc14bc109af0952bc5
phamvanthe94 <mihthe05121994@gmail.com>
2025-07-24 20:25:32 +0900

    Nộp bài tập SS11_ex03

---
+++ SS11_ex03.js
@@ -0,0 +1,36 @@
+let number = Number(prompt(" Mời bạn nhập vào số từ 0-->9"));
+
+switch (number) {
+  case 0:
+    alert("Sô Không");
+    break;
+  case 1:
+    console.log("Số Một");
+    break;
+  case 2:
+    console.log("Số Hai");
+    break;
+  case 3:
+    console.log("Số Ba");
+    break;
+  case 4:
+    console.log("Số Bốn");
+    break;
+  case 5:
+    console.log("Số Năm");
+    break;
+  case 6:
+    console.log("Số Sáu");
+    break;
+  case 7:
+    console.log("Số Bảy");
+    break;
+  case 8:
+    console.log("Số Tám");
+    break;
+  case 9:
+    console.log("Số Chín");
+    break;
+  default:
+    console.log("Chưa xử lý số lớn hơn 9");
+}
